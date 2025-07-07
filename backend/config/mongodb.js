03a791845c2a36d2b8cc86eec6e1144cec601773
Sayaib Sarkar <ssayaib@gmail.com>
2025-07-07 16:03:48 +0530

    r1
    fix some bugs of frontend

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   mongoose.connection.on("connected", () => console.log("Database Connected"));
-  await mongoose.connect(`${process.env.MONGODB_URI}/NARSO`);
+  await mongoose.connect(process.env.MONGODB_URI);
 };
 
 export default connectDB;
