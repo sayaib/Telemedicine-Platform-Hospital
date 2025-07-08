65a9a1ada075413f4a91da204ce05c2858f2924b
Sayaib Sarkar <ssayaib@gmail.com>
2025-07-08 18:10:32 +0530

    added tls

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   mongoose.connection.on("connected", () => console.log("Database Connected"));
-  await mongoose.connect(process.env.MONGODB_URI);
+  await mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ssl: true, // Make sure this is enabled
+  });
 };
 
 export default connectDB;
