4427252503b14f186f8f36efeb0e7d80211f61be
Sayaib Sarkar <ssayaib@gmail.com>
2025-07-09 11:38:55 +0530

    frontend dist folder connected to backend

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import path from "path";
+import { fileURLToPath } from "url";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/userRoute.js";
 import doctorRouter from "./routes/doctorRoute.js";
 import adminRouter from "./routes/adminRoute.js";
+
+// Setup for __dirname with ES Module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // app config
 const app = express();
@@ -17,18 +23,24 @@ connectCloudinary();
 app.use(express.json());
 app.use(
   cors({
-    origin: "*", // Or '*' for any origin in dev
-    credentials: true, // If you're sending cookies or auth headers
+    origin: "*",
+    credentials: true,
   })
 );
+
+// Serve static files from Vite build
+const viteBuildPath = path.join(__dirname, "dist");
+app.use(express.static(viteBuildPath));
+
 // api endpoints
 app.use("/api/user", userRouter);
 app.use("/api/admin", adminRouter);
 app.use("/api/doctor", doctorRouter);
 
-app.get("/", (req, res) => {
-  ``;
-  res.send("API Working");
+// Serve index.html for frontend routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(viteBuildPath, "index.html"));
 });
 
+// start server
 app.listen(port, () => console.log(`Server started on PORT:${port}`));
